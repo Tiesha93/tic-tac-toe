364e4bdd975305713d8ff327c9c38456336b8e31
github_Tiesha93 <_github_tieshawilliams93@icloud.com>
2023-11-26 22:55:07 -0700

    Added media query to make page responsive.

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ let currentPlayer = "X";
 let running = false;
 
 initializeGame();
-function initializeGame(){
+function initializeGame() {
     cells.forEach(cell => cell.addEventListener("click", cellClicked));
     restartBtn.addEventListener("click", restartGame);
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 }
+
 function cellClicked(){
     const cellIndex = this.getAttribute("cellIndex");
     if(options[cellIndex] != "" || ! running){
@@ -31,14 +32,17 @@ function cellClicked(){
     updateCell(this, cellIndex);
     checkWinner();
 }
+
 function updateCell(cell, index){
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
 }
+
 function changePlayer(){
     currentPlayer = (currentPlayer == "X") ? "O" : "X";
     statusText.textContent = `${currentPlayer}'s turn`;
 }
+
 function checkWinner(){
     let roundWon = false;
     for(let i = 0; i < winConditions.length; i++){
@@ -50,23 +54,28 @@ function checkWinner(){
         if(cellA == "" || cellB == "" || cellC == ""){
             continue;
         }
+
         if(cellA == cellB && cellB == cellC){
             roundWon = true;
             break;
         }
     }
+
     if(roundWon){
         statusText.textContent = `${currentPlayer} wins`;
         running = false;
     }
+
     else if(!options.includes("")){
         statusText.textContent = `DRAW!`;
         running = false;
     }
+
     else{
         changePlayer();
     }
 }
+
 function restartGame(){
     currentPlayer = "X";
     options = ["","","","","","","","",""];
